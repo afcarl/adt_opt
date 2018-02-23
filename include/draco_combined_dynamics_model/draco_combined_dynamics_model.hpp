3bb94d1193065257dd1e0cd7df1d7f76c3101cd1
Steven Jens Jorgensen <stevenjensj@gmail.com>
2018-02-23 13:09:28 -0600

    combined draco dynamics model

--- include/draco_combined_dynamics_model/draco_combined_dynamics_model.hpp
+++ include/draco_combined_dynamics_model/draco_combined_dynamics_model.hpp
@@ -2,15 +2,14 @@
 #define DRACO_P1_COMBINED_DYNAMICS_MODEL
 
 #include <Utils/wrap_eigen.hpp>
-#include <Utils/utilities.hpp>
 
 #include <draco_actuator_model/DracoActuatorModel.hpp>
 #include "DracoModel.hpp"
 
 class Draco_Combined_Dynamics_Model{
 public:
-	Draco_Combined_Dynamics_Model();
-	~Draco_Combined_Dynamics_Model();
+    static Draco_Combined_Dynamics_Model* GetDracoCombinedDynamicsModel();
+    ~Draco_Combined_Dynamics_Model(void);
 
 	DracoModel* robot_model;	
 	DracoActuatorModel* actuator_model;
@@ -20,11 +19,11 @@ public:
 	sejong::Matrix B_combined;
 	sejong::Matrix K_combined;
 
-	sejong::Vector A_mat;
-	sejong::Vector A_bb;
-	sejong::Vector A_br;
-	sejong::Vector A_brT;
-	sejong::Vector A_rr;
+	sejong::Vector A_mat; // NUM_QDOT x NUM_QDT
+	sejong::Vector A_bb; // NUM_VIRTUAL x NUM_VIRTUAL
+	sejong::Vector A_br; // NUM_VIRTUAL x NUM_ACT_JOINT
+	sejong::Vector A_brT; // NUM_ACT_JOINT x NUM_VIRTUAL
+	sejong::Vector A_rr; // NUM_ACT_JOINT x NUM_ACT_JOINT
 
 	sejong::Vector grav;
 	sejong::Vector coriolis;
@@ -36,6 +35,19 @@ public:
 	sejong::Matrix L; // dz/dq Jacobian;
 	sejong::Matrix J; // dq/dz Jacobian
 
+	sejong::Vector x_state;
+	sejong::Vector xdot_state;	
+
+	sejong::Vector q_state;
+	sejong::Vector qdot_state;	
+
+	sejong::Vector q_virt;
+	sejong::Vector qdot_virt;	
+	sejong::Vector q_act;
+	sejong::Vector qdot_act;
+
+	void UpdateModel(const sejong::Vector &x_state_in, const sejong::Vector &xdot_state_in);
+	void setContactJacobian(sejong::Matrix Jc_in);
 
 	void get_combined_mass_matrix(const sejong::Vector &x_state, const sejong::Vector &xdot_state, sejong::Matrix &M_out);
 	void get_combined_damping_matrix(const sejong::Vector &x_state, const sejong::Vector &xdot_state, sejong::Matrix &B_out);
@@ -45,4 +57,21 @@ public:
 	void get_actuated_joints_impedance(const sejong::Vector &x_state, const sejong::Vector &xdot_state, const sejong::Vector &Fr_states, sejong::Vector &sa_out);			
 
 	void get_state_acceleration(const sejong::Vector &x_state, const::sejong::Vector &xdot_state, sejong::Vector &xddot_state_out);
-}
+
+
+protected:
+	void convert_x_xdot_to_q_qdot(const sejong::Vector &x_state, const sejong::Vector &xdot_state);
+
+	void compute_mass_matrix();
+	void compute_damping_matrix();	
+	void compute_stiffness_matrix();
+	void compute_joint_link_impedance();			
+
+
+
+private:
+    Draco_Combined_Dynamics_Model();
+
+};
+
+#endif
---
+++ src/draco_combined_dynamics_model/draco_combined_dynamics_model.cpp
@@ -0,0 +1,94 @@
+#include <draco_combined_dynamics_model/draco_combined_dynamics_model.hpp>
+#include <Utils/utilities.hpp>
+#include "DracoP1Rot_Definition.h"
+
+
+Draco_Combined_Dynamics_Model* Draco_Combined_Dynamics_Model::GetDracoCombinedDynamicsModel(){
+    static Draco_Combined_Dynamics_Model draco_combined_dynamics_model_;
+    return & draco_combined_dynamics_model_;
+}
+
+Draco_Combined_Dynamics_Model::~Draco_Combined_Dynamics_Model(){
+	robot_model = DracoModel::GetDracoModel();
+	actuator_model = DracoActuatorModel::GetDracoActuatorModel();	
+}
+
+Draco_Combined_Dynamics_Model::Draco_Combined_Dynamics_Model(){
+	A_mat.resize(NUM_QDOT, NUM_QDOT); A_mat.setZero();
+	
+	A_bb.resize(NUM_VIRTUAL, NUM_VIRTUAL); A_bb.setZero();
+	A_br.resize(NUM_VIRTUAL, NUM_JOINT); A_br.setZero();
+	A_brT.resize(NUM_ACT_JOINT, NUM_VIRTUAL); A_brT.setZero();
+	A_rr.resize(NUM_ACT_JOINT, NUM_ACT_JOINT); A_rr.setZero();
+
+	grav.resize(NUM_QDOT);	grav.setZero();
+	coriolis.resize(NUM_QDOT); coriolis.setZero();
+
+	L.resize(NUM_ACT_JOINT, NUM_ACT_JOINT); L.setZero();
+	J.resize(NUM_ACT_JOINT, NUM_ACT_JOINT);	J.setZero();
+
+	Sv.resize(NUM_VIRTUAL, NUM_QDOT); Sv.setZero();
+	Sa.resize(NUM_ACT_JOINT, NUM_QDOT);	Sa.setZero();
+
+
+	M_combined.resize(NUM_QDOT + NUM_ACT_JOINT + NUM_ACT_JOINT, NUM_ACT_JOINT + NUM_ACT_JOINT); M_combined.setZero();
+	B_combined.resize(NUM_QDOT + NUM_ACT_JOINT + NUM_ACT_JOINT, NUM_QDOT + NUM_ACT_JOINT + NUM_ACT_JOINT); B_combined.setZero();
+	K_combined.resize(NUM_QDOT + NUM_ACT_JOINT + NUM_ACT_JOINT, NUM_QDOT + NUM_ACT_JOINT + NUM_ACT_JOINT); K_combined.setZero();
+
+	x_state.resize(NUM_QDOT + NUM_ACT_JOINT + NUM_ACT_JOINT); x_state.setZero();
+	xdot_state.resize(NUM_QDOT + NUM_ACT_JOINT + NUM_ACT_JOINT); xdot_state.setZero();
+
+	q_state.resize(NUM_QDOT); q_state.setZero();
+	qdot_state.resize(NUM_QDOT); qdot_state.setZero();
+
+	q_virt.resize(NUM_QDOT); q_virt.setZero();
+	qdot_virt.resize(NUM_QDOT); qdot_virt.setZero();	
+	q_act.resize(NUM_ACT_JOINT); q_act.setZero();
+	qdot_act.resize(NUM_ACT_JOINT); qdot_act.setZero();
+
+}
+
+void Draco_Combined_Dynamics_Model::UpdateModel(const sejong::Vector &x_state_in, const sejong::Vector &xdot_state_in){
+	x_state = x_state_in;
+	xdot_state = xdot_state_in;
+
+//	robot_model->UpdateModel()
+
+	compute_mass_matrix();
+	compute_damping_matrix();
+	compute_stiffness_matrix();
+	compute_joint_link_impedance();	
+}
+
+
+void Draco_Combined_Dynamics_Model::get_combined_mass_matrix(const sejong::Vector &x_state, const sejong::Vector &xdot_state, sejong::Matrix &M_out){}
+void Draco_Combined_Dynamics_Model::get_combined_damping_matrix(const sejong::Vector &x_state, const sejong::Vector &xdot_state, sejong::Matrix &B_out){}
+void Draco_Combined_Dynamics_Model::get_combined_stiffness_matrix(const sejong::Vector &x_state, sejong::Matrix &K_out){}
+void Draco_Combined_Dynamics_Model::get_virtual_joints_impedance(const sejong::Vector &x_state, const sejong::Vector &xdot_state, const sejong::Vector &Fr_states, sejong::Vector &sv_out){}
+void Draco_Combined_Dynamics_Model::get_actuated_joints_impedance(const sejong::Vector &x_state, const sejong::Vector &xdot_state, const sejong::Vector &Fr_states, sejong::Vector &sa_out){}			
+
+void Draco_Combined_Dynamics_Model::compute_mass_matrix(){
+}
+void Draco_Combined_Dynamics_Model::compute_damping_matrix(){
+}	
+void Draco_Combined_Dynamics_Model::compute_stiffness_matrix(){
+}
+void Draco_Combined_Dynamics_Model::compute_joint_link_impedance(){
+}
+
+
+void Draco_Combined_Dynamics_Model::convert_x_xdot_to_q_qdot(const sejong::Vector &x_state, const sejong::Vector &xdot_state){
+	sejong::Vector q_virt_state = x_state.head(NUM_VIRTUAL);
+	sejong::Vector z_state = x_state.segment(NUM_VIRTUAL, NUM_ACT_JOINT);
+	sejong::Vector delta_state = x_state.tail(NUM_ACT_JOINT);
+
+	sejong::Vector qdot_virt_state = xdot_state.head(NUM_VIRTUAL);
+	sejong::Vector zdot_state = xdot_state.segment(NUM_VIRTUAL, NUM_ACT_JOINT);
+	sejong::Vector delta_dot_state = xdot_state.tail(NUM_ACT_JOINT);	
+
+	sejong::Vector q_act;
+	sejong::Vector qdot_act;	
+
+/*	actuator_model->getFull_joint_pos_q(z_state, q_act);
+	actuator_model->getFull_joint_vel_qdot(zdot_state, qdot_act);	*/
+}
